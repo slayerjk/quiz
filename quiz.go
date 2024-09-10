74622a8c80a8e260bb7424ae3b5b628df80c9155
slayerjk <slayer.jk@mail.ru>
2024-09-10 11:52:45 +0500

    refactoring p.1

--- quiz.go
+++ quiz.go
@@ -3,146 +3,39 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
-type Quiz struct {
-	question string
-	answer   string
-}
-
 func main() {
 	timerCh := make(chan string)
-	quiz := Quiz{}
 	var (
-		quizeList                                         []Quiz
-		userAnswer, cleanedAnswer                         string
+		userAnswer                                        string
 		totalQuestions, answeredQuestions, correctAnswers int
-		csvFilePath                                       string = "problems.csv"
-		timerSeconds                                      string = "30s"
-		optionsSummary                                    string
-		appArgs                                           = make(map[string]string)
 	)
-	helpMsg := `This is a Quiz program written in GO as a task from "https://github.com/gophercises/quiz".
 
-Help:
+	// defining flags
+	csvFilePath := flag.String("f", "problems.csv", "set csv('question,answer') file path; default: 'problems.csv'")
+	timerSeconds := flag.Int("t", 30, "time execution limit, seconds; default: 30")
+	// isShuffled := flag.Bool("s", false, "shuffle quiz file questions order; default: false")
+	flag.Parse()
 
-NO ARGS: run program with default parameters:
-	- default "problem.csv" quiz questions csv file located in the same directorty as the quiz main program;
-	- 30 secs for execution (countdown timer);
-	- no shuffle for quiz questions 
-
--h: print the help info and EXIT program
--f FILEPATH: use different .csv file(must have two columns without header: "question,answer")
--t SECONDS: custom countdown timer, in seconds; can be used with "-f" separately
--s: turn Shuffle of quiz questions ON
-
-Examples:
-	quiz # run program with default ./problems.csv and 30 seconds for timer
-	quiz -f test/test.csv # run program with test/test.csv as csv input file
-	quiz -f test/test.csv -t 60 # run program with test/test.csv as csv input file and 60 secs for timer
-	quiz -t 60 # run program with 60 secs for timer
-	quiz -t 60 -s # run program with 60 secs for timer and turn shuffle on
------`
-
-	// getting all args
-	for i, v := range os.Args {
-		if i == 0 {
-			continue
-		} else {
-			switch {
-			case v == "-h":
-				fmt.Println(helpMsg)
-				os.Exit(0)
-			case v == "-s":
-				appArgs[v] = ""
-				optionsSummary += "Shuffle quiz questions is: On\n"
-			case v == "-f" || v == "-t":
-				if os.Args[i+1] != "" {
-					if v == "-f" {
-						appArgs[v] = ""
-						csvFilePath = os.Args[i+1]
-						optionsSummary += fmt.Sprintf("Using %s as csv file path\n", csvFilePath)
-					} else if v == "-t" {
-						appArgs[v] = "ON"
-						timerSeconds = os.Args[i+1] + "s"
-						optionsSummary += fmt.Sprintf("Using %s for timer\n", timerSeconds)
-					} else {
-						log.Fatalf("Empty arg for %s", v)
-					}
-				}
-			default:
-				continue
-			}
-		}
-	}
-
-	// check if shuffle is off for default message
-	if _, ok := appArgs["-s"]; !ok {
-		optionsSummary += "Shuffle quiz questions is: Off(Default)\n"
-	}
-	// check if -f options true
-	if _, ok := appArgs["-f"]; !ok {
-		optionsSummary += fmt.Sprintf("Using Default %s as csv file path\n", csvFilePath)
-	}
-	// check if -t options true
-	if _, ok := appArgs["-t"]; !ok {
-		optionsSummary += fmt.Sprintf("Using %s for timer\n", timerSeconds)
-	}
-
-	// reading quiz csv file
-	csvFile, err := os.Open(csvFilePath)
+	// parsing csv file
+	problems, err := parseCsv(*&csvFilePath)
 	if err != nil {
-		log.Fatalf("failed to open quiz csv file:\n\t%v", err)
+		log.Fatalf("failed to parse csv file %s:\n\t%v", *csvFilePath, err)
 	}
-	defer csvFile.Close()
-
-	csvReader := csv.NewReader(csvFile)
-	csvReader.FieldsPerRecord = 2
-
-	csvData, err := csvReader.ReadAll()
-	if err != nil {
-		log.Fatalf("failed to read from csvReader:\n\t%v", err)
-	}
+	totalQuestions = len(problems)
 
 	// printing app intro info & options selected
-	fmt.Println(`This is a Quiz program written in GOas a task from "https://github.com/gophercises/quiz".
+	fmt.Println("This is a Quiz program written in GOas a task from 'https://github.com/gophercises/quiz'.\n-----")
 
-	Use "-h" flag to print help.
------`)
-	fmt.Println(optionsSummary)
-
-	// parsing and checking csv file
-	for _, v := range csvData {
-		quiz.question = v[0]
-		quiz.answer = v[1]
-
-		// answer or question in csv can't be empty
-		if quiz.question == "" || quiz.answer == "" {
-			log.Fatalf("quiz answer or question can't be empty,\n\t check this entry: %v", v)
-		}
-		quizeList = append(quizeList, quiz)
-		totalQuestions++
-	}
-
-	// making order of quiz questions
-	indexes := make([]int, 0)
-	println(indexes)
-
-	// if -s is ON then make slice of random unique indexes
-	if appArgs["-s"] != "ON" {
-		for i := 0; i < len(quizeList); i++ {
-			indexes = append(indexes, i)
-		}
-		fmt.Println(indexes)
-	} else {
-
-	}
-	os.Exit(0)
+	// TODO: making order of quiz questions
 
 	// Wait user to start quiz
 	fmt.Println("Press ENTER when you're ready: ")
@@ -154,11 +47,16 @@ Examples:
 
 	// Start timer
 	go func() {
-		duration, err := time.ParseDuration(timerSeconds)
+		// first convert timer seconds given in -s flag(30 default) to string to parse time duration
+		timerSecondsString := strconv.Itoa(*timerSeconds)
+
+		// setting duration for timer
+		duration, err := time.ParseDuration(timerSecondsString + "s")
 		if err != nil {
 			log.Fatalf("FAILED: to parse duration from user input:\n\t%v", err)
 		}
 
+		// starting timer, sending msg to channel when expired
 		timer := time.NewTimer(duration)
 		<-timer.C
 		timerCh <- "Timer is EXPIRED!"
@@ -166,7 +64,7 @@ Examples:
 
 	// quiz loop
 	fmt.Println("Quiz is started!")
-	for _, v := range quizeList {
+	for _, v := range problems {
 		select {
 		case msg := <-timerCh:
 			fmt.Println(msg)
@@ -177,13 +75,41 @@ Examples:
 			fmt.Print("Your answer: ")
 			fmt.Scan(&userAnswer)
 
-			// clean up an answer(trim trailing whitespaces, convert to lower)
-			cleanedAnswer = strings.ToLower(strings.Trim(userAnswer, " "))
-			if cleanedAnswer == v.answer {
+			if *&userAnswer == v.answer {
 				correctAnswers++
 			}
 			answeredQuestions++
 		}
 	}
 	fmt.Printf("\nYour result is %d correct answers of %d answered of %d total questions!\n", correctAnswers, answeredQuestions, totalQuestions)
+}
+
+type quiz struct {
+	question string
+	answer   string
+}
+
+func parseCsv(path *string) ([]quiz, error) {
+	file, err := os.Open(*path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open csv file %s:\n\t%v", *path, err)
+	}
+
+	reader := csv.NewReader(file)
+
+	csvData, err := reader.ReadAll()
+	if err != nil {
+		return nil, fmt.Errorf("failed to read csv file %s:\n\t%v", *path, err)
+	}
+
+	result := make([]quiz, len(csvData))
+
+	for i, v := range csvData {
+		result[i] = quiz{
+			question: v[0],
+			answer:   strings.TrimSpace(v[1]),
+		}
+	}
+
+	return result, nil
 }
